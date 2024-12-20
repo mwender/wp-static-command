e53e219cc7ffe278205c9e88bdb79ebe80375473
Michael Wender <michael@michaelwender.com>
2024-12-20 14:21:17 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+![WP Static Command](https://raw.githubusercontent.com/mwender/wp-static-command/main/bin/thumbnail.png)
+
+# WP Static Command
+
+Create a static version of your WordPress posts and pages.
+
+Running `wp static <post_id>` saves your post or page as a static HTML page inside the root directory of your WordPress install.
+
+## Installation
+
+`wp package install mwender/wp-static-command`
+
+## Usage
+
+Create a static version of a post or page:
+
+`wp static <post_id>`
+
+## Changelog
+
+_1.0.0_
+
+- Initial release.
+
+## License
+
+This package is licensed under the MIT license.
---
+++ bin/wp-static.php
@@ -0,0 +1,85 @@
+<?php
+// Register the WP-CLI command
+if (defined('WP_CLI') && WP_CLI) {
+    WP_CLI::add_command('static', 'Static_Command');
+}
+
+class Static_Command {
+
+    /**
+     * Generates a static HTML file for a given post/page.
+     *
+     * ## OPTIONS
+     *
+     * <post_id>
+     * : The ID of the post/page to generate a static file for.
+     *
+     * ## EXAMPLES
+     *
+     *     wp static 21
+     *
+     * @param array $args Command arguments.
+     * @param array $assoc_args Associative arguments.
+     */
+    public function __invoke($args, $assoc_args) {
+        list($post_id) = $args;
+
+        // Validate post ID
+        $post = get_post($post_id);
+        if (!$post || !in_array($post->post_status, ['publish', 'private'])) {
+            WP_CLI::error("Post with ID {$post_id} not found or is not published/private.");
+        }
+
+        // Get the permalink and prepare the file path
+        $permalink = get_permalink($post_id);
+        if (!$permalink) {
+            WP_CLI::error("Could not generate permalink for post ID {$post_id}.");
+        }
+
+        $parsed_url = parse_url($permalink);
+        $path = trim($parsed_url['path'], '/');
+        $static_dir = ABSPATH . $path;
+
+        if (!file_exists($static_dir)) {
+            wp_mkdir_p($static_dir);
+        }
+
+        $html_file = trailingslashit($static_dir) . 'index.html';
+
+        // Fetch the post content
+        $html_content = $this->get_static_content($post_id);
+
+        // Save the HTML content
+        if (file_put_contents($html_file, $html_content) === false) {
+            WP_CLI::error("Failed to write static file at {$html_file}.");
+        }
+
+        WP_CLI::success("Static file generated: {$html_file}");
+    }
+
+    /**
+     * Retrieves the static HTML content for a post/page.
+     *
+     * @param int $post_id The ID of the post/page.
+     * @return string The static HTML content.
+     */
+    private function get_static_content($post_id) {
+        // Use output buffering to capture the rendered HTML
+        ob_start();
+
+        // Set up the main query for the specified post
+        query_posts(['p' => $post_id, 'post_type' => 'any']);
+
+        // Include the theme's template loader to render the content
+        include ABSPATH . WPINC . '/template-loader.php';
+
+        // Get the output and clean up
+        $content = ob_get_clean();
+
+        // Reset the query
+        wp_reset_query();
+
+        return $content;
+    }
+}
+
